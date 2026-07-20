e65e1632ec72f8e5f3dc351d64ab06601a624909
RamishRaz <ramishnaveed52@gmail.com>
2026-07-20 03:34:25 -0700

    Improve the Product Model

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, product_detail
 
 urlpatterns = [
     path("model/<int:model_id>/", product_list, name="product_list"),
+    path("detail/<int:product_id>/", product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -5,15 +5,26 @@ from .models import Product
 
 def product_list(request, model_id):
     car_model = get_object_or_404(CarModel, id=model_id)
-
-    products = Product.objects.filter(
-        car_model=car_model,
-        is_active=True
-    )
+    products = Product.objects.filter(car_model=car_model).order_by("name")
 
     context = {
         "car_model": car_model,
         "products": products,
     }
 
-    return render(request, "products/product_list.html", context)
+    return render(request, "products/product.html", context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    related_products = Product.objects.filter(
+        category=product.category
+    ).exclude(id=product.id)[:4]
+
+    context = {
+        "product": product,
+        "related_products": related_products,
+    }
+
+    return render(request, "products/product_detail.html", context)

afb5a1d2ada6de51275f6386b4961a3e3d96d37b
RamishRaz <ramishnaveed52@gmail.com>
2026-07-20 17:40:46 -0700

    Professional Homepage Redesign

--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from brands.models import Brand
+from products.models import Category, Product
 
 
 def home(request):
     brands = Brand.objects.all()
+    categories = Category.objects.all()[:8]
+    featured_products = Product.objects.filter(is_active=True)[:8]
 
     context = {
-        "brands": brands
+        "brands": brands,
+        "categories": categories,
+        "featured_products": featured_products,
     }
 
     return render(request, "home.html", context)

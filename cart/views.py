a2582ed064d411c3ea11a7e1bcdf63e8ffbe2eef
RamishRaz <ramishnaveed52@gmail.com>
2026-07-21 19:25:06 -0700

    Complete Shopin Cart Functionality

--- cart/views.py
+++ cart/views.py
@@ -56,3 +56,31 @@ def remove_from_cart(request, product_id):
     request.session["cart"] = cart
 
     return redirect("cart")
+def increase_quantity(request, product_id):
+    cart = request.session.get("cart", {})
+
+    product_id = str(product_id)
+
+    if product_id in cart:
+        cart[product_id] += 1
+
+    request.session["cart"] = cart
+
+    return redirect("cart")
+
+
+def decrease_quantity(request, product_id):
+    cart = request.session.get("cart", {})
+
+    product_id = str(product_id)
+
+    if product_id in cart:
+
+        if cart[product_id] > 1:
+            cart[product_id] -= 1
+        else:
+            del cart[product_id]
+
+    request.session["cart"] = cart
+
+    return redirect("cart")
